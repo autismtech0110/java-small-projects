2fd1f6c85cce95c90b3f638ec8e035c6a9aa351c
autismtech0110 <107578323+autismtech0110@users.noreply.github.com>
2025-04-06 16:56:38 -0700

    Worked on the board-games code, not finished yet I think.

--- board-games/BoardGames.java
+++ board-games/BoardGames.java
@@ -1,59 +1,61 @@
 public class BoardGames {
-   public static void main(String[] args) {
-       //do not touch this code
-       Monopoly monopoly = new Monopoly();
-       Chess chess = new Chess();
-       Battleships battleships = new Battleships();
-       
-       monopoly.play();
-       chess.play();
-       battleships.play();
-   }
+   
+    public static void main(String[] args) {
+        // Declare class objects
+	Monopoly m = new Monopoly();
+        Chess c = new Chess();
+        Battleship b = new Battleship();
+        
+	// Objects can now call their class methods
+        m.play();
+        c.play();
+        b.play();
+    }
 }
 
 abstract class Game {
-   abstract String getName();
-   abstract void play();
+    abstract String getName();
+    abstract void play();
 }
 
 class Monopoly extends Game {
 
-   //give "Monopoly" name to game
-   String getName() {
-       String name = "Monopoly";
-       return name;
+    // Give "Monopoly" name to game
+    String getName() {
+        String name = "Monopoly";
+        return name;
    }
 
-   // play method should print "Buy all property."
-   void play() {
-       System.out.println("Buy all property.");
-   }
+    // Play method should print "Buy all property."
+    void play() {
+        System.out.println("Buy all property.");
+    }
 }
 
 class Chess extends Game {
 
-   //give "Chess" name to game
-   String getName() {
-       String name = "Chess";
-       return name;
-   }
+    // Give "Chess" name to game
+    String getName() {
+        String name = "Chess";
+        return name;
+    }
 
-   // play method should print "Kill the enemy king."
+   // Play method should print "Capture the enemy king."
    void play() {
-       System.out.println("Kill the enemy king.");
+       System.out.println("Capture the enemy king.");
    }
 }
 
-class Battleships extends Game {
+class Battleship extends Game {
 
-   //give "Battleships" name to game
-   String getName() {
-       String name = "Battleships";
-       return name;
-   }
+    // Give "Battleship" name to game
+    String getName() {
+        String name = "Battleship";
+        return name;
+    }
 
-   // play method should print "Sink all ships."
-   void play() {
-       System.out.println("Sink all ships.");
-   }
+    // Play method should print "Sink all ships."
+    void play() {
+        System.out.println("Sink all ships.");
+    }
 }
